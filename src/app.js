ba54849280e76dcb945bc1d16f836b604bd736ea
Kashish <kash1ish2114@gmail.com>
2026-01-16 11:21:08 +0530

    Multiple Route handlers

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ const express = require("express");
 
 const app = express();
  
-app.use("/test",(req,res)=>{
-    res.send("Hello from test");
-})
-
-app.use("/hello",(req,res)=>{
-    res.send("Hello");
-})
-
-app.use("/",(req,res)=>{
-    res.send("Hello from dashboard");
+app.use("/user",(req,res,next)=>{
+    //route handler
+    //app.use can handle any method
+    next();
+    console.log("1")
+   // res.send("route handler 1");
+},(req,res,next)=>{
+    console.log("2")
+    //res.send("route handler 2");
+    next();
 })
 
 app.listen(3000);
